894ee6818b72f4ba343d7076a1a5c07e0774e97f
benn <benjacoblee@gmail.com>
2020-11-04 12:25:16 +0800

    add command to remove existing feed file

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Removing existing feed file.."
+rm /mnt/c/Users/User/Documents/code/projects/python-aggregator/feed.html
 echo "Beginning execution"
 source /home/benn/.virtualenvs/my_django_env/bin/activate
 python3 /mnt/c/Users/User/Documents/code/projects/python-aggregator/aggregate.py
 python3 /mnt/c/Users/User/Documents/code/projects/python-aggregator/send_mail.py
 echo "Script completed"
+rm /mnt/c/Users/User/Documents/code/projects/python-aggregator/feed.html
+echo "Cleanup complete"
