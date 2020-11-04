0e14393328b641483eb1b3683201d45600a824f0
benn <benjacoblee@gmail.com>
2020-11-04 12:27:00 +0800

    fix script file

--- scripts.sh
+++ scripts.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "Removing existing feed file.."
-rm /mnt/c/Users/User/Documents/code/projects/python-aggregator/feed.html
-echo "Beginning execution"
+echo "Executing script.."
 source /home/benn/.virtualenvs/my_django_env/bin/activate
 python3 /mnt/c/Users/User/Documents/code/projects/python-aggregator/aggregate.py
 python3 /mnt/c/Users/User/Documents/code/projects/python-aggregator/send_mail.py
